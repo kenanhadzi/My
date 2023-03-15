542dc35ebea71726858d55d36ce5b9ef93bbd828
kenanhadzi <124839049+kenanhadzi@users.noreply.github.com>
2023-03-15 18:58:08 +0100

    Add files via upload

---
+++ vazno22.cy.js
@@ -0,0 +1,23 @@
+import AutoStore_Homepage_PO from "../PageObjects/AutoStore_Homepage_PO.js"
+
+
+describe("Add multiple items to basket", () => {
+  const autoStore_Homepage_PO = new AutoStore_Homepage_PO();
+
+  before(function () {
+    cy.fixture("products").then(function (data) {
+      globalThis.data = data;
+    });
+  });
+
+  beforeEach(function () {
+    autoStore_Homepage_PO.accessHomepage();
+    autoStore_Homepage_PO.clickOn_HairCare_Link();
+  });
+  it("Add specific items to basket", () => {
+      globalThis.data.productName.forEach(function(element) {
+          cy.addProductToBasket(element)
+      })
+      cy.get('.dropdown-toggle > .fa').click();
+  });
+});
