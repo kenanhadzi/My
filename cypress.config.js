cadfa4f1fb77159868e6e9e6f6c1f2591cf42549
Kenan Hadzimahmutovic <124839049+kenanhadzi@users.noreply.github.com>
2023-03-16 18:08:29 +0100

    Add files via upload

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,21 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      screenshotOnRunFailure=true;
-      require('cypress-mochawesome-reporter/plugin')(on);
     },
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}",
+    excludeSpecPattern: "cypress/e2e/other/*.js",
+    baseUrl: "http://www.webdriveruniversity.com",
+    chromeWebSecurity: false,
+    experimentalSessionAndOrigin: true,
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 120000,
+    env: {
+      first_name: "Sarah",
+      webdriveruni_homepage: "http://www.webdriveruniversity.com"
+    }
   },
 });
+
